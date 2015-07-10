b1cde963133ad6ee86ab5e0d60129982e290be2a
David Christ <chd+dev@genitopia.org>
2015-07-11 01:18:46 +0200

    Make simple fetching work

---
+++ app.js
@@ -0,0 +1,43 @@
+(function (global) {
+    var addTagForUrl,
+        buildUrl,
+        handler,
+        initialize,
+        output,
+        scriptTag;
+
+    addTagForUrl = function addTagForUrl(url) {
+        document.getElementsByTagName('body')[0].appendChild(scriptTag(url));
+    };
+
+    buildUrl = function buildUrl(url) {
+        var base, query, params;
+        base = 'https://query.yahooapis.com/v1/public/yql?q=';
+        query = 'select * from xml where url="' + url + '"';
+        params = '&format=json&diagnostics=false&callback=feedMe.handler';
+        console.log(base + encodeURIComponent(query) + params);
+        return base + encodeURIComponent(query) + params;
+    };
+    
+    handler = function handler(result) {
+        console.log(result);
+        output.innerHTML = JSON.stringify(result.query.results);
+    };
+
+    initialize = function initialize() {
+        global.feedMe = global.feedMe || {};
+        global.feedMe.handler = global.feedMe.handler || handler;
+
+        output = document.getElementById('output');
+        addTagForUrl(buildUrl('http://www.slideshare.net/rss/user/nashjain'));
+    };
+
+    scriptTag = function scriptTag(url) {
+        var result = document.createElement('script');
+        result.setAttribute('src', url);
+        return result;
+    };
+
+    initialize();
+
+}(this));
