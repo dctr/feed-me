1f04055f76cfd3d2a1db13614d1f166a6db4e45b
David Christ <chd+dev@genitopia.org>
2015-08-04 14:07:15 +0200

    Add eslinting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,26 @@
 module.exports = function Gruntfile(grunt) {
   'use strict';
 
-  grunt.registerTask('default', []);
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    eslint: {
+      target: ['src/**.js']
+    },
+    babel: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'dist/app.js': 'src/app.js'
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', [
+    'eslint',
+    'babel'
+  ]);
 };
--- src/app.js
+++ src/app.js
@@ -1,43 +1,43 @@
 (function (global) {
-    var addTagForUrl,
-        buildYqlUrl,
-        handler,
-        initialize,
-        output,
-        scriptTag;
+  var addTagForUrl,
+    buildYqlUrl,
+    handler,
+    initialize,
+    output,
+    scriptTag;
 
-    addTagForUrl = function addTagForUrl(url) {
-        document.getElementsByTagName('body')[0].appendChild(scriptTag(url));
-    };
+  addTagForUrl = function addTagForUrl(url) {
+    document.getElementsByTagName('body')[0].appendChild(scriptTag(url));
+  };
 
-    buildYqlUrl = function buildYqlUrl(url) {
-        var base, query, params;
-        base = 'https://query.yahooapis.com/v1/public/yql?q=';
-        query = 'select * from xml where url="' + url + '"';
-        params = '&format=json&diagnostics=false&callback=feedMe.handler';
-        console.log(base + encodeURIComponent(query) + params);
-        return base + encodeURIComponent(query) + params;
-    };
+  buildYqlUrl = function buildYqlUrl(url) {
+    var base, query, params;
+    base = 'https://query.yahooapis.com/v1/public/yql?q=';
+    query = 'select * from xml where url="' + url + '"';
+    params = '&format=json&diagnostics=false&callback=feedMe.handler';
+    console.log(base + encodeURIComponent(query) + params);
+    return base + encodeURIComponent(query) + params;
+  };
 
-    handler = function handler(result) {
-        console.log(result);
-        output.innerHTML = JSON.stringify(result.query.results);
-    };
+  handler = function handler(result) {
+    console.log(result);
+    output.innerHTML = JSON.stringify(result.query.results);
+  };
 
-    initialize = function initialize() {
-        global.feedMe = global.feedMe || {};
-        global.feedMe.handler = global.feedMe.handler || handler;
+  initialize = function initialize() {
+    global.feedMe = global.feedMe || {};
+    global.feedMe.handler = global.feedMe.handler || handler;
 
-        output = document.getElementById('output');
-        addTagForUrl(buildYqlUrl('http://rss.golem.de/rss.php?feed=ATOM1.0'));
-    };
+    output = document.getElementById('output');
+    addTagForUrl(buildYqlUrl('http://rss.golem.de/rss.php?feed=ATOM1.0'));
+  };
 
-    scriptTag = function scriptTag(url) {
-        var result = document.createElement('script');
-        result.setAttribute('src', url);
-        return result;
-    };
+  scriptTag = function scriptTag(url) {
+    var result = document.createElement('script');
+    result.setAttribute('src', url);
+    return result;
+  };
 
-    initialize();
+  initialize();
 
 }(this));
