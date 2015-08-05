15a525960b23b238d0a861235aa8f29350797e70
David Christ <chd+dev@genitopia.org>
2015-08-05 07:48:20 +0200

    Refactor

--- src/feedFactory.js
+++ src/feedFactory.js
@@ -1,55 +1,64 @@
-const jsonPHandlerPrefix = 'feedMeJsonP';
+const jsonPCallbackPrefix = 'feedMeJsonP';
 const jsonPTimeout = 10000;
 
 export function getFeed(url) {
-  const promiseKey = encodeURIComponent(url);
-  let feedPromiseResolve;
   let appendedChild;
   let body = document.getElementsByTagName('body')[0];
+  let resolvePromise;
+  let now = new Date();
+
+  const jsonPCallback = jsonPCallbackPrefix + now.getMinutes().toString() + now.getSeconds().toString() + now.getMilliseconds().toString();
 
   function cleanUp(url) {
-    delete window[jsonPHandlerPrefix + promiseKey];
+    delete window[jsonPCallback];
     body.removeChild(appendedChild);
   }
 
-  function addTagForUrl(url) {
+  function addScriptTagForUrl(url) {
     appendedChild = body.appendChild(createScriptTag(url));
   }
 
   function jsonPHandler(result) {
+    console.log('Received feed');
     console.log(result);
-    feedPromiseResolve(JSON.stringify(result.query.results));
+    resolvePromise(JSON.stringify(result.query.results));
   }
 
-  window[jsonPHandlerPrefix + promiseKey] = window[jsonPHandlerPrefix + promiseKey] || jsonPHandler;
+  window[jsonPCallback] = window[jsonPCallback] || jsonPHandler;
 
   return new Promise((resolve, reject) => {
     let resolved = false;
 
-    feedPromiseResolve = function resolveWrapper(data) {
+    console.log('Build feed for: ' + url + ' with key ' + jsonPCallback);
+
+    resolvePromise = function resolvePromise(data) {
       resolved = true;
+      // TODO: Resolve with parsed data (either RSS class or ATOM class).
       resolve(data);
       cleanUp(url);
     };
 
-    setTimeout(function rejectAfterTimeout() {
-      if (!resolved) {
-        reject();
-        cleanUp(url);
-      }
-    }, jsonPTimeout);
+    setTimeout(
+      function rejectAfterTimeout() {
+        if (!resolved) {
+          reject();
+          cleanUp(url);
+        }
+      },
+      jsonPTimeout
+    );
 
-    addTagForUrl(buildYqlUrl(url));
+    addScriptTagForUrl(buildYqlUrl(url, jsonPCallback));
 
   });
 
 }
 
-function buildYqlUrl(url) {
-  var base, query, params;
-  base = 'https://query.yahooapis.com/v1/public/yql?q=';
-  query = 'select * from xml where url="' + url + '"';
-  params = '&format=json&diagnostics=false&callback=feedMe.handler';
+function buildYqlUrl(url, callback) {
+  const base = 'https://query.yahooapis.com/v1/public/yql?q=';
+  let query = 'select * from xml where url="' + url + '"';
+  let params = '&format=json&diagnostics=false&callback=' + callback;
+
   console.log(base + encodeURIComponent(query) + params);
   return base + encodeURIComponent(query) + params;
 }
