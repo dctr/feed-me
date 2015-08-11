46e81f32d681dc0aae77dc07319cb6b329efbcbc
David Christ <chd+dev@genitopia.org>
2015-08-11 16:36:49 +0200

    Implement grunt watch (no server yet)

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,59 +3,98 @@ module.exports = function Gruntfile(grunt) {
 
   require('load-grunt-tasks')(grunt);
 
-  grunt.initConfig({
-    babel: {
-      options: {
-        modules: 'system',
-        sourceMap: true
+  grunt.initConfig(
+    {
+      babel: {
+        options: {
+          modules: 'system',
+          sourceMap: true
+        },
+        dist: {
+          files: [
+            {
+              expand: true,
+              cwd: 'src/',
+              src: ['**/*.js'],
+              dest: 'dist/'
+            }
+          ]
+        }
       },
-      dist: {
-        files: [
-          {
-            expand: true,
-            cwd: 'src/',
-            src: ['**/*.js'],
-            dest: 'dist/'
+      clean: ["dist"],
+      copy: {
+        markup: {
+          files: [
+            {
+              expand: true,
+              cwd: 'src/',
+              src: ['**/*.html'],
+              dest: 'dist/'
+            }
+          ]
+        }
+      },
+      eslint: {
+        target: ['src/**.js']
+      },
+      watch: {
+        bower: {
+          files: ['bower.json'],
+          tasks: ['wiredep']
+        },
+        gruntfile: {
+          files: ['Gruntfile.js']
+        },
+        livereload: {
+          files: ['dist/**/*'],
+          options: {
+            livereload: true
           }
-        ]
-      }
-    },
-    clean: ["dist"],
-    copy: {
-      html: {
-        files: [
-          {
-            expand: true,
-            cwd: 'src/',
-            src: ['**/*.html'],
-            dest: 'dist/'
-          }
-        ]
-      }
-    },
-    eslint: {
-      target: ['src/**.js']
-    },
-    wiredep: {
-      task: {
-        src: ['dist/**/*.html']
+        },
+        markup: {
+          files: ['src/**/*.html'],
+          tasks: ['copy:markup', 'wiredep']
+        },
+        scripts: {
+          files: ['src/**/*.js'],
+          tasks: ['babel']
+        }
+      },
+      wiredep: {
+        task: {
+          src: ['dist/**/*.html']
+        }
       }
     }
-  });
+  );
 
-  grunt.registerTask('test', [
-    'eslint'
-  ])
+  grunt.registerTask(
+    'build', [
+      'clean',
+      'copy',
+      'babel',
+      'wiredep'
+    ]
+  );
 
-  grunt.registerTask('build', [
-    'clean',
-    'copy',
-    'babel',
-    'wiredep'
-  ]);
+  grunt.registerTask(
+    'develop',
+    [
+      'build',
+      'watch'
+    ]
+  );
 
-  grunt.registerTask('default', [
-    'test',
-    'build'
-  ]);
+  grunt.registerTask(
+    'test', [
+      'eslint'
+    ]
+  );
+
+  grunt.registerTask(
+    'default', [
+      'test',
+      'build'
+    ]
+  );
 };
