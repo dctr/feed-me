d0170f7e5a9c3bf4f430ac3385a7b3e8c00ef0dc
David Christ <chd+dev@genitopia.org>
2015-09-10 12:30:29 +0200

    Fix typo

--- src/app.js
+++ src/app.js
@@ -98,20 +98,3 @@ main = () => {
 };
 
 main();
-
-// myDefaultFeeds = {
-//  "feeds": [
-//    "http://rss.golem.de/rss.php?feed=ATOM1.0",
-//    "http://ticker.gulli.com/rss",
-//    "http://www.bbc.co.uk/blogs/doctorwho/atom",
-//    "http://www.heise.de/developer/rss/news-atom.xml",
-//    "http://www.heise.de/netze/rss/netze-atom.xml",
-//    "http://www.heise.de/newsticker/heise-atom.xml",
-//    "http://www.heise.de/open/news/news-atom.xml",
-//    "http://www.heise.de/security/news/news-atom.xml",
-//    "http://www.pro-linux.de/rss/1/4/atom_alles.xml",
-//    "https://mailbox.org/feed/",
-//    "https://www.archlinux.org/feeds/news/",
-//    "https://www.tagesschau.de/xml/rss2"
-//  ]
-// }
